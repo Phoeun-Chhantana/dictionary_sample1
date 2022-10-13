5ad1ec93a02f4c70d78b4e5b6de4a19b24f3dc8b
Phoeun-Chhantana <42382202+Phoeun-Chhantana@users.noreply.github.com>
2022-10-13 14:05:21 +0700

    deny space on textfield

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "0a9e7b52688529c7fdac8e0911508a38",
-"index.html": "d5c9a19736967cd59053552e5fdce3de",
-"/": "d5c9a19736967cd59053552e5fdce3de",
-"main.dart.js": "58bdd4541ae273dcf4168208e17e59a8",
+"index.html": "993459afdc8149e32bc7d364e076822f",
+"/": "993459afdc8149e32bc7d364e076822f",
+"main.dart.js": "5853bb68f93d6b53bb03cb2ac2cabf50",
 "flutter.js": "f85e6fb278b0fd20c349186fb46ae36d",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
